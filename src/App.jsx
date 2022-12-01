1085e2ec34ebeb5922a6de5b5ec49ab556336481
alexandru-w98 <stoica.alex.w98@gmail.com>
2022-12-01 14:56:57 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+import Input from "./components/input";
+
+const App = () => {
+  return (
+    <div className={styles["test"]}>
+      <Input />
+    </div>
+  );
+};
+
+export default App;
